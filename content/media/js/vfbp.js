4adea24a793b1afb6ca3d659bb270e3284f4362d
Kwpolska <kwpolska@gmail.com>
2012-04-24 15:06:07 +0200

    debugging.

--- content/media/js/vfbp.js
+++ content/media/js/vfbp.js
@@ -39,6 +39,7 @@ function vfbpauth() {
 	$.post("http://kwpolska.tk/contact/antibot.php",
 			{'json': '{"vfbpjs": "auth", "token" = "'+$('[name="vfbpjstoken"]').val()+'"}'},
 			function(data) {
+				echo(data);
 				if(data.auth == true) {
 					$('#vfbpjsresults').html = 'Authenticated as a human being! (or a bot with JS support)';
 					$('[name="vfbpq"]').val('[ignored]');
