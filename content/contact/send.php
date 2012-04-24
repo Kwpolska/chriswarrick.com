3dd195d7e06f1c1643e6767a4b7e95afd5bb59e3
Kwpolska <kwpolska@gmail.com>
2012-04-24 16:22:58 +0200

    ah yes.

--- content/contact/send.php
+++ content/contact/send.php
@@ -13,10 +13,6 @@ echo "<strong>Checking anti-bot protection... [VFBPJS v2]</strong>";
 /*if ($captcha != md5($_POST['captcha'])) showError("This is not the right
 CAPTCHA phrase."); //md5 = idiot-proof*/
 
-if ($_SESSION['vfbpjstoken'] != $_POST['vfbpjstoken']) {
-    showError("DO NOT HACK THE PAGE SOURCE!  As a punishment, not sending.".$_SESSION['vfbpjstoken'].' vs '.$_POST['vfbpjstoken']);
-}
-
 if ($_SESSION['vfbpjsauth'] != true && $_GET['vfbpq'] != $_SESSION['vfbpfnum']) {
     showError("No valid authentication.");
 }
