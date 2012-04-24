31c85db0b13452b4df22980a847c85e74764bb70
Kwpolska <kwpolska@gmail.com>
2012-04-24 13:52:09 +0200

    Fixing some problems with the new contact form.

--- content/contact/form.php
+++ content/contact/form.php
@@ -6,7 +6,7 @@ description: Want to talk to me?  Go for it.  Here’s the right form.
 ---
 <form action="/contact/send.php" method="post" id="form">
     <article>
-        <header><label for="name">Name</label><input id="name" name="name" required class="required"><br><label for="mail">Mail address</label><input id="mail" name="mail" type="email" required class="email required"><br>
+        <header><label for="name">From <label for="mail">&lt;mail&gt;</label>:<input id="name" name="name" required class="required" style="width: 40%;"> &lt;<input id="mail" name="mail" type="email" required class="email required" style="width: 40%;">&gt;<br>
             <input name="subject" id="subject" required class="required" style="font-weight: bold; font-family: 'Play',sans-serif; font-size: 2em;">
         </header>
         <div class="text clearfix"><textarea name="message" id="message" rows="24" required class="required" style="width:100%;"></textarea></div>
