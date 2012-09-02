df3446a194fbc85861bdf7f976080e943661011e
Kwpolska <kwpolska@gmail.com>
2012-09-02 14:06:26 +0200

    a lite style, for some use

--- content/kwbot/archive/trollops.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "<strong>Funkcje operatorskie KwBota wykorzystywać mogą:</strong> ";
-$ops = file_get_contents('/home/Kwpolska/kwbot/conf/trollowniaops.txt');
-$ruskie = array('а', 'о', 'ϳ');
-$polske = array('a', 'o', 'j');
-$ops = str_replace($ruskie, $polske, $ops);
-echo $ops;
-?>
