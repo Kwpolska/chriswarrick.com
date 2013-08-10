7d33168c8b9e6325df17c3605fa6c17c5c6ef955
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2013-08-10 11:16:07 +0200

    add NFSN deployment; remove PHP scripts
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

--- conf.py
+++ conf.py
@@ -163,7 +163,7 @@ REDIRECTIONS = []
 # "rsync -rav output/* joe@my.site:/srv/www/site"
 # And then do a backup, or ping pingomatic.
 # To do manual deployment, set it to []
-DEPLOY_COMMANDS = []
+DEPLOY_COMMANDS = ['rsync -rav output/* kwpolska_kwpolska@ssh.phx.nearlyfreespeech.net:/home/public']
 
 # Where the output site should be located
 # If you don't use an absolute path, it will be considered as relative
--- files/php/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (false) {
-    echo 'If you can see this, PHP IS BROKEN!'
-} else {
-    echo 'I am forced to use PHP for ceratin stuff, and all my PHP is held here.'
-}
-?>
