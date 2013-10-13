69c18c350b4c663c6b9fa2a71ad4c75b151f64b9
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2013-10-13 11:46:35 +0200

    Remove the virtualenv.  I’m using virtualenvwrapper for this now.
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

--- lib/python2.7/site-packages/doit/__main__.py
+++
@@ -1,8 +0,0 @@
-# lazy way to ignore coverage in this file
-if True: # pragma: no cover
-    import sys
-
-    from doit.doit_cmd import DoitMain
-
-    sys.exit(DoitMain().run(sys.argv[1:]))
-
--- lib/python2.7/site-packages/pudb/__main__.py
+++
@@ -1,3 +0,0 @@
-if __name__ == "__main__":
-    from pudb.run import main
-    main()
