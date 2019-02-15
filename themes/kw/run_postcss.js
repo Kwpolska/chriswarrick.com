18957e68b9363b2dffcfc5fce8a16c0ba4fd39c5
Chris Warrick <kwpolska@gmail.com>
2019-02-15 19:13:39 +0100

    Drop Gulp (less dependencies, cleaner pipeline)

---
+++ themes/kw/run_postcss.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const getStdin = require('get-stdin');
+
+const postcss = require('postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+const name = process.argv[2];
+
+getStdin().then(css => {
+    postcss([autoprefixer]).process(css, {from: undefined}).then(result1 => {
+        fs.writeFileSync(`assets/css/${name}.css`, result1.css);
+
+        postcss([cssnano]).process(result1.css, {from: undefined}).then(result2 => {
+            fs.writeFileSync(`assets/css/${name}.min.css`, result2.css)
+        });
+    });
+});
--- themes/kw/update-bootstrap.sh
+++ themes/kw/update-bootstrap.sh
@@ -1,2 +1,3 @@
-#!/bin/bash
-npx gulp
+#!/bin/bash -v
+sass bootstrap-kw.scss | node run_postcss.js bootstrap
+sass bootstrap-kw-dark.scss | node run_postcss.js bootstrap-dark
