38dadf4f0878b152e4f78501fc44a08c4df63149
Chris Warrick <kwpolska@gmail.com>
2024-01-01 17:41:04 +0100

    2024! :fireworks:

--- conf.py
+++ conf.py
@@ -373,7 +373,7 @@ LICENSE = """<a rel="license" href="https://creativecommons.org/licenses/by-nc-s
 CONTENT_FOOTER = {
     'en': """<p>this site uses cookies.<br>by using this site, you agree to our <a href="/privacy/">cookie and privacy policy</a>.</p><p>
 powered by <strong><a href="https://getnikola.com/">Nikola</a></strong>, <a href="http://nginx.org/">nginx</a> and <a href="https://hetzner.cloud/?ref=Qy1lehF8PwzP">Hetzner Cloud</a><br>
-copyright © 2009–2023 <a
+copyright © 2009–2024 <a
 href="/contact/">Chris Warrick</a><br> <a rel="license"
 href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC
 BY-NC-SA</a>, unless stated otherwise</p>""",
@@ -381,7 +381,7 @@ BY-NC-SA</a>, unless stated otherwise</p>""",
 korzystając z tej strony, akceptujesz naszą <a href="/pl/privacy/">politykę prywatności i ciasteczek</a>.
 </p><p>
 powered by <strong><a href="https://getnikola.com/">Nikola</a></strong>, <a href="http://nginx.org/">nginx</a> and <a href="https://hetzner.cloud/?ref=Qy1lehF8PwzP">Hetzner Cloud</a><br>
-copyright © 2009–2023 <a
+copyright © 2009–2024 <a
 href="/pl/contact/">Chris Warrick</a><br> <a rel="license"
 href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC
 BY-NC-SA</a>, unless stated otherwise</p>""",
